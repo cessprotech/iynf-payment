3a5db112ccd0bdbd8c32727037fb483d8b10ec85
chucky-golden <chukschibyke92@gmail.com>
2024-07-15 19:51:04 +0100

    test

--- src/app/schema/externals.schema.ts
+++ src/app/schema/externals.schema.ts
@@ -3,6 +3,7 @@ import mongoose, { Schema } from "mongoose";
 export const ExternalModels = [
     { name: 'User', schema: new Schema({}) },
     { name: 'Job', schema: new Schema({}) },
+    { name: 'Influencer', schema: new Schema({}) },
     { name: 'Event', schema: new Schema({}) },
     { name: 'Participant', schema: new Schema({}) },
 ] 
